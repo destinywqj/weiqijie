0b13cfc8e5ede3ce1b80ef0f52b1d5a32345b432
destinywqj <52655176+destinywqj@users.noreply.github.com>
2019-08-16 11:36:10 +0800

    Update main.c

--- main.c
+++ main.c
@@ -11,12 +11,12 @@ LED_Init();
 while(1){
 
 GPIO_SetBits(GPIOB,GPIO_Pin_5);
-GPIO_SetBits(GPIOE,GPIO_Pin_5);
-delay_ms(500);
+GPIO_ResetBits(GPIOE,GPIO_Pin_5);
+delay_ms(300);
 	
 	GPIO_ResetBits(GPIOB,GPIO_Pin_5);
-  GPIO_ResetBits(GPIOE,GPIO_Pin_5);
-delay_ms(500);
+  GPIO_SetBits(GPIOE,GPIO_Pin_5);
+delay_ms(300);
 
 }
 
